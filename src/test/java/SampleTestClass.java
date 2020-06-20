e23a9d6239cafd99cc5182fb08c0d3aae8f5537a
Ruslana <ruslana.chumachenko@gmail.com>
2020-06-20 11:58:14 +0300

    fixed test

--- src/test/java/SampleTestClass.java
+++ src/test/java/SampleTestClass.java
@@ -6,25 +6,26 @@ import org.testng.annotations.Test;
 
 public class SampleTestClass {
 
-  WebDriver driver = null;
+    WebDriver driver = null;
 
-  @BeforeTest()
-  public void setUp() {
-    // Open the browser
-    WebDriverFactory.createInstance("Chrome");
-  }
+    @BeforeTest()
+    public void setUp() {
+        // Open the browser
+        WebDriverFactory.createInstance("Chrome");
+        driver = WebDriverFactory.getDriver();
+    }
 
-  @Test
-  public void sampleTestMethod() {
-    driver.get("https://www.google.com/");
-    driver.findElement(By.cssSelector("input[name='q']")).sendKeys("Artur");
-    assert 1 == 1;
-  }
+    @Test
+    public void sampleTestMethod() {
+        driver.get("https://www.google.com/");
+        driver.findElement(By.cssSelector("input[name='q']")).sendKeys("Artur");
+        assert 1 == 1;
+    }
 
-  @AfterTest()
-  public void tearDown() {
-    // Close the browser
-    WebDriverFactory.getDriver().quit();
-  }
+    @AfterTest()
+    public void tearDown() {
+        // Close the browser
+        WebDriverFactory.getDriver().quit();
+    }
 
 }
